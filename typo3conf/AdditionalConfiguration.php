7b4d25d329b71d653fca6ba4e8c4dc7bf3570cfb
bunsin <kimbunsin003@gmail.com>
2016-09-08 08:52:47 +0700

    I would love to add new file to control security from hacker

---
+++ typo3conf/AdditionalConfiguration.php
@@ -0,0 +1,9 @@
+<?php
+
+if (! defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'website_camemis';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = '127.0.0.1';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'root';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = '';
---
+++ typo3conf/LocalConfiguration.php
@@ -0,0 +1,78 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$P$C4JioDsYT2P7w9jVQveQxVsPfgzdLr0',
+        'loginSecurityLevel' => 'rsa',
+        'versionNumberInFilename' => '0',
+    ],
+    'DB' => [
+        //'host' => '127.0.0.1',
+        //'password' => '',
+        'port' => 3306,
+        //'username' => 'root',
+    ],
+    'EXT' => [
+        'extConf' => [
+            'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+            'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+        ],
+    ],
+    'FE' => [
+        'debug' => '1',
+        'loginSecurityLevel' => 'rsa',
+    ],
+    'GFX' => [
+        'jpg_quality' => '80',
+    ],
+    'INSTALL' => [
+        'wizardDone' => [
+            'TYPO3\CMS\Install\Updates\AccessRightParametersUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\BackendUserStartModuleUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\Compatibility6ExtractionUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\ContentTypesToTextMediaUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\FileListIsStartModuleUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\FilesReplacePermissionUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\LanguageIsoCodeUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\MediaceExtractionUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\MigrateMediaToAssetsForTextMediaCe' => 1,
+            'TYPO3\CMS\Install\Updates\MigrateShortcutUrlsAgainUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\OpenidExtractionUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\PageShortcutParentUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\ProcessedFileChecksumUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\TableFlexFormToTtContentFieldsUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\WorkspacesNotificationSettingsUpdate' => 1,
+            'TYPO3\CMS\Rtehtmlarea\Hook\Install\DeprecatedRteProperties' => 1,
+            'TYPO3\CMS\Rtehtmlarea\Hook\Install\RteAcronymButtonRenamedToAbbreviation' => 1,
+        ],
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'extbase_object' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+                    'groups' => [
+                        'system',
+                    ],
+                    'options' => [
+                        'defaultLifetime' => 0,
+                    ],
+                ],
+            ],
+        ],
+        'clearCacheSystem' => '1',
+        'devIPmask' => '',
+        'displayErrors' => '1',
+        'enableDeprecationLog' => false,
+        'encryptionKey' => '355e0d3d70a730df3f77aee0bcc1c4464ff82aa5034f7dd6d79c453eb46c6ae0d469e70ec65a3f49d65c36de9f0da311',
+        'isInitialDatabaseImportDone' => true,
+        'isInitialInstallationInProgress' => false,
+        'sitename' => 'camemis_website',
+        'sqlDebug' => '1',
+        'systemLogLevel' => 2,
+        't3lib_cs_convMethod' => 'mbstring',
+        't3lib_cs_utils' => 'mbstring',
+    ],
+];
